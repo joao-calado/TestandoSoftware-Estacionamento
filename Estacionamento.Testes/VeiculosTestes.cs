8e5af88987f2dfb096178abf47261cbad8c955fc
joao-calado <joaovsscalado@gmail.com>
2023-11-02 18:11:44 -0300

    ClassData e InlineData

--- Estacionamento.Testes/VeiculosTestes.cs
+++ Estacionamento.Testes/VeiculosTestes.cs
@@ -4,7 +4,8 @@ namespace Estacionamento.Testes
 {
     public class VeiculosTestes
     {
-        [Fact]
+        [Fact(DisplayName = "Teste nº1")]
+        [Trait("Funcionalidade", "Acelerar")]
         public void TestaVeiculoAcelerar()
         {
             //Arrange
@@ -15,7 +16,8 @@ namespace Estacionamento.Testes
             Assert.Equal(100, veiculo.VelocidadeAtual);
         }
 
-        [Fact]
+        [Fact(DisplayName = "Teste nº2")]
+        [Trait("Funcionalidade", "Frear")]
         public void TestaVeiculoFrear()
         {
             //Arrange
@@ -25,5 +27,27 @@ namespace Estacionamento.Testes
             //Assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
         }
+
+        [Fact(DisplayName = "Teste nº3", Skip = "Teste ainda não implementado. Ignorar")]
+        public void ValidaNomeProprietario()
+        {
+
+        }
+
+        [Theory]
+        [ClassData(typeof(Veiculo))]
+        public void TestaVeiculoClass(Veiculo modelo)
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+
+            //Act
+            veiculo.Acelerar(10);
+            modelo.Acelerar(10);
+
+            //Assert
+            Assert.Equal(modelo.VelocidadeAtual, veiculo.VelocidadeAtual);
+        }
+
     }
 }
