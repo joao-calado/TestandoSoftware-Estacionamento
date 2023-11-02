971957450d6b104d65e376be4c765dbff6c0dc29
joao-calado <joaovsscalado@gmail.com>
2023-11-02 15:07:07 -0300

    Aula #02 - Utilizando xUnit

---
+++ Estacionamento.Testes/PatioTestes.cs
@@ -0,0 +1,35 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Testes
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = "João Calado";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Vermelho";
+            veiculo.Modelo = "Gol Quadrado";
+            veiculo.Placa = "bzn-2486";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
