2e6dea8ed21e33cd2cdc8395b53d3e8b82defe12
joao-calado <joaovsscalado@gmail.com>
2023-11-02 14:53:43 -0300

    Aula #01

---
+++ Estacionamento.Testes/VeiculosTestes.cs
@@ -0,0 +1,29 @@
+using Alura.Estacionamento.Modelos;
+
+namespace Estacionamento.Testes
+{
+    public class VeiculosTestes
+    {
+        [Fact]
+        public void TestaVeiculoAcelerar()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Acelerar(10);
+            //Assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaVeiculoFrear()
+        {
+            //Arrange
+            var veiculo = new Veiculo();
+            //Act
+            veiculo.Frear(10);
+            //Assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+    }
+}
